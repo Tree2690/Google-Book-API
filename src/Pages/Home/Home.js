c711727a3d73961b16182c6494e953af3f76346e
Jonathan Liang <jonathanliang2690123@gmail.com>
2020-07-15 23:51:00 -0700

    WIP

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import CardHeader from '@material-ui/core/CardHeader'
+import axios from 'axios'
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  })
+
+const Home =()=>
+{
+    const classes = useStyles()
+
+    const 
+
+}
+
+export default Home
